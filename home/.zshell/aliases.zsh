f771d67cad37dd17732dd972281f804826167716
Richard Tynan <richard@tynan.me>
2014-09-21 21:44:26 +0100

    Added to aliases and fixed typo

--- home/.zshell/aliases.zsh
+++ home/.zshell/aliases.zsh
@@ -7,6 +7,7 @@ alias xflux="xflux -l 53.4129 -g -8.2439"
 alias xclip="xclip -selection clipboard"
 
 # Conviences
+alias his="history"
 alias dutreesize="du --max-depth=1 -h"
 alias grepsauce="ps aux | grep "
 alias setus="setxkbmap -layout 'us' && setxkbmap -option caps:escape"
