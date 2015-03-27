a238db44e166caada22844881748eaefaf3c65db
Richard Tynan <richard@tynan.me>
2015-03-27 22:31:10 +0000

    Fixed mistake in install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,9 @@ else
     echo -ne "\x1B[33m~/Update prezto?[yn]\x1B[39m"
     read -n 1
     echo ""
-    zsh -c "cd ~/.zprezto && git pull && git submodule update --init --recursive"
+    if [[ $REPLY =~ [yY] ]]; then
+        zsh -c "cd ~/.zprezto && git pull && git submodule update --init --recursive"
+    fi
 fi
 
 echo -ne "\x1B[33m~/Install NeoBundle?[yn]\x1B[39m"
