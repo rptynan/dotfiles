abcddccd8bc676ef6200433c9d36f25c5efcbf4c
Richard Tynan <richard@tynan.me>
2025-07-23 12:35:31 +0100

    Add fidget indicator to nvim codecompanion

--- home/.config/nvim/lua/plugins/codecompanion.lua
+++ home/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,42 +1,46 @@
 return {
-	"olimorris/codecompanion.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function()
-		require("codecompanion").setup({
-			strategies = {
-				chat = {
-					adapter = "openai",
-				},
-				inline = {
-					adapter = "openai",
-				},
-			},
-			adapters = {
-				openai = function()
-					return require("codecompanion.adapters").extend("openai", {
-						opts = {
-							stream = true,
-						},
-						env = {
-							api_key = "cmd: cat ~/.config/openai.token",
-						},
-						schema = {
-							model = {
-								default = function()
-									return "gpt-4.1"
-								end,
-							},
-						},
-					})
-				end,
-			},
-		})
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "j-hui/fidget.nvim",
+  },
+  init = function()
+    require("plugins.codecompanion.fidget-spinner"):init()
+  end,
+  config = function()
+    require("codecompanion").setup({
+      strategies = {
+        chat = {
+          adapter = "openai",
+        },
+        inline = {
+          adapter = "openai",
+        },
+      },
+      adapters = {
+        openai = function()
+          return require("codecompanion.adapters").extend("openai", {
+            opts = {
+              stream = true,
+            },
+            env = {
+              api_key = "cmd: cat ~/.config/openai.token",
+            },
+            schema = {
+              model = {
+                default = function()
+                  return "gpt-4.1"
+                end,
+              },
+            },
+          })
+        end,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>c", ":CodeCompanionChat<CR>", { noremap = true, silent = true })
-		-- Expand 'cc' into 'CodeCompanion' in the command line
-		vim.cmd([[cab cc CodeCompanion]])
-	end,
+    vim.keymap.set("n", "<leader>c", ":CodeCompanionChat<CR>", { noremap = true, silent = true })
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd([[cab cc CodeCompanion]])
+  end,
 }
