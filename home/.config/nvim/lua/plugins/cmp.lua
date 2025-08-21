39b05b9177863b02d770995de35fbeedd0eaffb2
Richard Tynan <richard@tynan.me>
2025-08-21 21:36:39 +0100

    cmp tweaks

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,34 +1,41 @@
 -- Adds support for more LSP capabilities.
 return {
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-		"windwp/nvim-autopairs",
-		"hrsh7th/cmp-buffer",
-	},
-	config = function()
-		local cmp = require("cmp")
-		cmp.setup({
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "buffer" },
-			}),
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				-- C-n and C-p scroll entries themselves.
-				-- Scroll in docs window.
-				["<C-u>"] = cmp.mapping.scroll_docs(-4),
-				["<C-d>"] = cmp.mapping.scroll_docs(4),
-				-- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-		})
-		local autopairs = require("nvim-autopairs")
-		autopairs.setup({})
-	end,
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "windwp/nvim-autopairs",
+    "hrsh7th/cmp-buffer",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      performance = {
+        -- Can be quite distracting to have completions firing up constantly, so trying to turn them down a bit.
+        debounce = 300,
+        throttle = 300,
+      },
+      --completion = {
+      --	--autocomplete = false,
+      --},
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+      }),
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        -- C-n and C-p work for selecting options. Scroll in docs window.
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-space>"] = cmp.mapping.confirm({ select = true }),
+      }),
+    })
+
+    local autopairs = require("nvim-autopairs")
+    autopairs.setup({})
+  end,
 }
